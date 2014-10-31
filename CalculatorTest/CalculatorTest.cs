4c9a7c987ff1be167e18886a7c41ea8c76fa0873
minhaz082 <minhaz082@hotmail.com>
2014-10-31 08:54:44 +0600

    unit test task in class

---
+++ CalculatorTest/CalculatorTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using UnitTestApp;
+
+namespace CalculatorTest
+{
+    [TestFixture]
+  public  class CalculatorTest
+    {
+        [Test]
+
+      public void ADDTEST_CHECK_IF_CAN_ADD_TWO_POSITIVE_NUMBERS()
+      {
+          Calculator aCalculator=new Calculator();
+          
+          Assert.AreEqual(-5.07, aCalculator.Add(-2.00,-3.07));
+      }
+    }
+}
---
+++ UnitTestApp/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestApp
+{
+   public class Calculator
+    {
+       public double Add(double firstNumber, double secondNumber)
+       {
+           return firstNumber + secondNumber;
+       }
+    }
+}
